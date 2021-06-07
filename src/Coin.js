78bf7838ffc8938c9efe5256f429ffd1872bb14e
shvashakidze <shvashakidze@users.noreply.github.com>
2021-06-06 19:11:09 -0700

    mobile screen

--- src/Coin.js
+++ src/Coin.js
@@ -3,8 +3,11 @@ import React from 'react'
 const  Coin =({name, image, symbol, price, volume, priceChange, marketcap}) =>{
     return (
         <div className='coin-container'>
-            <div className='coin-row'>
+            
+            <div className='coin-row' >
+            
                 <div className='coin'>
+                    
             <img src={image} alt='crypto'/>
             <h1>{name}</h1>
             <p className='coin-symbol'>{symbol}</p>
